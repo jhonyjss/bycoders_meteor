a217ce9be60deb5295e512b23e806601f5717ba5
Jhony Souza <jhony.jss@gmail.com>
2024-01-16 10:29:28 -0300

    first commity

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <h1>teste</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import {
+  RouteObject,
+  createBrowserRouter
+} from "react-router-dom";
+import App from "../App";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: App()
+  }
+];
+
+const router = createBrowserRouter(routes);
+
+export default router;
